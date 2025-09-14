d813904b81dc7adc51302adad166cec77edb43fd
Ivan Demkov <vanya.demkov@gmail.com>
2025-09-14 13:45:16 +0300

    working routing, homepage and posts page beginning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Posts } from './pages/posts/posts';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
+  { path: 'posts', component: Posts },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
