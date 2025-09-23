c605cb71c7b5f9afa8b8e7395c8c76d7cc10ab83
Ivan Demkov <vanya.demkov@gmail.com>
2025-09-23 17:19:46 +0300

    added template pages with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Home } from './pages/home/home';
 import { Posts } from './pages/posts/posts';
 import { Accounts } from './pages/accounts/accounts';
 import { Tasks } from './pages/tasks/tasks';
+import { Logs } from './pages/logs/logs';
+import { Settings } from './pages/settings/settings';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'posts', component: Posts },
   { path: 'accounts', component: Accounts },
   { path: 'tasks', component: Tasks },
+  { path: 'logs', component: Logs },
+  { path: 'settings', component: Settings },
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
